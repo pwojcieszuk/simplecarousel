c95ae5675ee529833e5858db86d00f3fa90c6b9b
Piotr Wojcieszuk <piotr.wojcieszuk@scrive.com>
2021-04-17 13:01:57 +0200

    Boilerplate - Webpack, React, Babel, Sass etc.

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "components/Carousel/carousel.module.scss";
+
+const Carousel: React.FC = () => (
+  <h1 className={styles.red}>Simple carousel</h1>
+);
+
+export default Carousel;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Carousel from "components/Carousel";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Carousel />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
